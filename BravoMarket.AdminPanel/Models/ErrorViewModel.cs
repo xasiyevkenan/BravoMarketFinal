f06f2e87bc887319ac569cd951d924a9ae8de983
kenanxasiyevv <kananakh@code.edu.az>
2023-09-08 19:25:36 +0400

    secondcommit

--- BravoMarket.AdminPanel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BravoMarket.AdminPanel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
